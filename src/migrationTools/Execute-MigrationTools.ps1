c93a98671a04b47eaefea650e50ca1759fd45fa8
Martin Hnshelwood <martin@nkdagility.com>
2024-06-06 17:59:54 +0000

    Update

--- src/migrationTools/Execute-MigrationTools.ps1
+++ src/migrationTools/Execute-MigrationTools.ps1
@@ -1,6 +1,6 @@
 # Migrate the Team Project Data using the appropriate config file (work items, plans with or without, queries, etc.)
 cls
-$pathToConfig = Get-Item -Path ".\output\validus\projects\VCAPS2-ModelFile\workItemsOnly-custom.json" -ErrorAction Stop 
+$pathToConfig = Get-Item -Path ".\output\validus\projects\VCAPS2-Contract\workItemsOnly.json" -ErrorAction Stop 
 $($pathToConfig.FullName)
 
 . C:\tools\MigrationTools\devopsmigration.exe execute --config "$($pathToConfig.FullName)"
--- src/processFieldMigrator/Generate-ProjectStats.ps1
+++ src/processFieldMigrator/Generate-ProjectStats.ps1
@@ -33,18 +33,35 @@ foreach ($org in $config.organisations) {
     $projects = Invoke-RestMethod -Uri $projectsURL -Method Get -ContentType "application/json" -Headers $header
 
     $csv = @"
-organisation,project,workItems, sharedsteps,pipelines,plans,suites,repos, area, inlcude, priority`n
+organisation,project,process base, process,workItems, sharedsteps,pipelines,plans,suites,repos, area, inlcude, priority`n
 "@
 
 
     foreach ($project in $projects.value) {
         Write-InfoLog $project.name
+        $Process = $null;
+        $ProcessBase = $null;
+        $WorkItemCount = $null;
+        $WorkItemCount2 = $null;
+
+        $propertiesURL = "$($org.url)/_apis/projects/$($project.id)/properties?keys=*process*&api-version=7.2-preview.1"
+        Write-DebugLog $propertiesURL
+        $queryResults = $null
+        $queryResults = Invoke-RestMethod -Uri $propertiesURL -Method Get -ContentType "application/json" -Headers $header
+        $ProcessBase = ($queryResults.value | Where-Object { $_.name -eq 'System.Process Template' } | select -Property value).value
+        Write-InfoLog "    '$ProcessBase' base process"
+        $ProcessTemplateId =  ($queryResults.value | Where-Object { $_.name -eq 'System.ProcessTemplateType' } | select -Property value).value
+        $processURL = "$($org.url)/_apis/work/processes/$($ProcessTemplateId)?api-version=7.2-preview.2"
+        $queryResults = $null
+        $queryResults = Invoke-RestMethod -Uri $processURL -Method Get -ContentType "application/json" -Headers $header
+        $Process = $queryResults.name
+        Write-InfoLog "    '$Process' process"
 
         # POST https://dev.azure.com/{organization}?api-version=7.0
         $wiqlURL = "$($org.url)/$($project.id)/_apis/wit/wiql?$queryString"
         Write-DebugLog $wiqlURL
 
-        $BODY = '{ "query": "Select [System.Id], [System.Title], [System.State] From WorkItems where [System.TeamProject] = ''' + $($project.name) + ''' order by [System.CreatedDate] desc"}' | ConvertFrom-Json -Depth 100
+        $BODY = '{ "query": "Select [System.Id], [System.Title], [System.State], [System.Rev] From WorkItems where [System.TeamProject] = ''' + $($project.name) + ''' AND [System.WorkItemType] NOT IN (''Test Suite'', ''Test Plan'',''Shared Steps'',''Shared Parameter'',''Feedback Request'') ORDER BY [System.ChangedDate] desc"}' | ConvertFrom-Json -Depth 100
         $queryResults = $null
         $queryResults = Invoke-RestMethod -Uri $wiqlURL -Method Post -ContentType "application/json" -Headers $header -Body ($BODY | ConvertTo-Json -Depth 10)
         $WorkItemCount = 0
@@ -54,7 +71,7 @@ organisation,project,workItems, sharedsteps,pipelines,plans,suites,repos, area, 
             [int]$queryYear = Get-Date -Format "yyyy"
             while ($queryHasResults)
             {
-                $BODY = '{ "query": "Select [System.Id], [System.Title], [System.State] From WorkItems where [System.ChangedDate] >= ''01-01-' + $queryYear + ''' AND [System.ChangedDate] <= ''01-01-' + ($queryYear+1) + ''' AND [System.TeamProject] = ''' + $($project.name) + ''' order by [System.CreatedDate] desc"}' | ConvertFrom-Json -Depth 100
+                $BODY = '{ "query": "Select [System.Id], [System.Title], [System.State], [System.Rev] From WorkItems where [System.CreatedDate] >= ''01-01-' + $queryYear + ''' AND [System.CreatedDate] <= ''01-01-' + ($queryYear+1) + ''' AND [System.TeamProject] = ''' + $($project.name) + ''' AND [System.WorkItemType] NOT IN (''Test Suite'', ''Test Plan'',''Shared Steps'',''Shared Parameter'',''Feedback Request'') ORDER BY [System.ChangedDate] desc"}' | ConvertFrom-Json -Depth 100
                 $queryResults = $null
                 $queryResults = Invoke-RestMethod -Uri $wiqlURL -Method Post -ContentType "application/json" -Headers $header -Body ($BODY | ConvertTo-Json -Depth 10)
                 $WorkItemCount += $queryResults.workItems.Count
@@ -81,18 +98,21 @@ organisation,project,workItems, sharedsteps,pipelines,plans,suites,repos, area, 
         #GET https://dev.azure.com/{organization}/{project}/_apis/pipelines?api-version=7.0
         $pipelinesURL = "$($org.url)/$($project.id)/_apis/pipelines?$queryString"
         Write-DebugLog $pipelinesURL
+        $pipelinesResults = $null;
         $pipelinesResults = Invoke-RestMethod -Uri $pipelinesURL -Method Get -ContentType "application/json" -Headers $header
         Write-InfoLog "    $($pipelinesResults.Count) Pipelines"
 
         #GET https://dev.azure.com/{organization}/{project}/_apis/testplan/plans?api-version=7.0
         $plansURL = "$($org.url)/$($project.id)/_apis/testplan/plans?$queryString"
         Write-DebugLog $plansURL
+        $plansResults = $null;
         $plansResults = Invoke-RestMethod -Uri $plansURL -Method Get -ContentType "application/json" -Headers $header
         Write-InfoLog "    $($plansResults.Count) Plans"
 
         #GET https://dev.azure.com/{organization}/{project}/_apis/git/repositories?api-version=7.1-preview.1
         $reposURL = "$($org.url)/$($project.id)/_apis/git/repositories?$queryString"
         Write-DebugLog $reposURL
+        $reposResults = $null;
         $reposResults = Invoke-RestMethod -Uri $reposURL -Method Get -ContentType "application/json" -Headers $header
         Write-InfoLog "    $($reposResults.Count) Repos"
 
@@ -110,7 +130,7 @@ organisation,project,workItems, sharedsteps,pipelines,plans,suites,repos, area, 
 
 
 
-        $csv += "$($org.url),$($project.name),$WorkItemCount, $WorkItemCount2,$($pipelinesResults.Count),$($plansResults.Count),$totalSuits,$($reposResults.Count)`n"
+        $csv += "$($org.url),$($project.name),$ProcessBase, $Process,$WorkItemCount, $WorkItemCount2,$($pipelinesResults.Count),$($plansResults.Count),$totalSuits,$($reposResults.Count)`n"
     }
 
     $sanitisedOrgname = $($org.url).Replace("https://dev.azure.com/", "").Replace("visualstudio.com/", "").Replace("/", "")
